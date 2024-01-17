e8e8082aa9ef828a091088a91c1fe263ecba078c
Fatih-01 <fatihazbentt@gmail.com>
2024-01-17 20:29:31 +1100

    Created server.py for front end

--- server.py
+++ server.py
@@ -2,12 +2,12 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route('/process_input', methods=['POST'])
-def process_input():
-    user_input = request.form['user_input']
+@app.route('/search', methods=['POST'])
+def search():
+    user_input = request.form['productName']
     # Do something with the user_input (e.g., print it)
     print("User input:", user_input)
-    return "Input received successfully!"
+    return user_input
 
 @app.route('/')
 def mockup():
