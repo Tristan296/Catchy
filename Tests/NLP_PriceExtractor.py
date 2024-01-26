cd829ad08e2127eb5e337d7d3934681ba7e50bf7
Tristan N <109927879+Tristan296@users.noreply.github.com>
2024-01-26 17:36:38 +1100

    Improved training for NLP - Increased accuracy

--- Tests/NLP_PriceExtractor.py
+++ Tests/NLP_PriceExtractor.py
@@ -7,29 +7,22 @@ import spacy
 from spacy.training.example import Example
 from spacy.training import offsets_to_biluo_tags
 
+from fuzzywuzzy import fuzz
+
 # Load spaCy model
 nlp = spacy.load("en_core_web_sm")
 
 # Annotated examples
 TRAIN_DATA = [
-     ("Add MADDOX Mason Geo Print Short Sleeve Shirt in Navy to wishlist$69.95$41.97", {"entities": [(4, 53, "PRODUCT"), (65, 76, "MONEY")]}),
-     ("X-Series Portable Wireless Speaker SRSXE300H in Grey to wishlist$319.00$249.00", {"entities": [(0, 56, "PRODUCT"), (81, 87, "MONEY")]}),
-    ("Sony X-Series Portable Wireless Speaker SRSXE300H in Grey to wishlist$319.00$249.00", {"entities": [(0, 56, "PRODUCT"), (75, 80, "MONEY"), (81, 87, "MONEY")]}),
-    ("Sony X-Series Portable Wireless Speaker SRSXE300B in Black to wishlist$319.00$249.00", {"entities": [(0, 56, "PRODUCT"), (75, 80, "MONEY"), (81, 87, "MONEY")]}),
-    ("Sony X-Series Portable Wireless Speaker SRSXE200H in Grey to wishlist$249.00$189.00", {"entities": [(0, 55, "PRODUCT"), (74, 79, "MONEY"), (80, 86, "MONEY")]}),
-    ("Sony X-Series Portable Wireless Speaker SRSXE200L in Blue to wishlist$249.00$189.00", {"entities": [(0, 54, "PRODUCT"), (73, 78, "MONEY"), (79, 85, "MONEY")]}),
-    ("Sony X-Series Portable Wireless Speaker SRSXG300B in Black to wishlist$479.00$383.20", {"entities": [(0, 54, "PRODUCT"), (73, 78, "MONEY"), (79, 85, "MONEY")]}),
-    ("Sony X-Series Portable Wireless Speaker SRSXG300H in Grey to wishlist$479.00$383.20", {"entities": [(0, 54, "PRODUCT"), (73, 78, "MONEY"), (79, 85, "MONEY")]}),
-    ("Sony Linkbuds S WFLS900NB in Black to wishlist$349.95$279.96", {"entities": [(0, 47, "PRODUCT"), (66, 71, "MONEY"), (72, 78, "MONEY")]}),
-    ("Sony Sony Black On Ear Noise Cancelling Headphones MDRZX110NC to wishlist$99.95$79.95", {"entities": [(0, 72, "PRODUCT"), (91, 96, "MONEY"), (97, 103, "MONEY")]}),
-    ("Sony Black In Ear Headphones MDREX15AP to wishlist$29.95$19.95", {"entities": [(0, 46, "PRODUCT"), (65, 70, "MONEY"), (71, 77, "MONEY")]}),
-    ("Sony White In Ear Headphones MDREX155AP to wishlist$54.95$32.97", {"entities": [(0, 47, "PRODUCT"), (66, 71, "MONEY"), (72, 78, "MONEY")]}),
-    ("Sony Black In Ear Headphones MDRE9LPB to wishlist$14.95$9.95", {"entities": [(0, 47, "PRODUCT"), (66, 71, "MONEY"), (72, 78, "MONEY")]}),
-    ("Sony Blue In Ear Headphones MDREX15AP to wishlist$29.95$19.95", {"entities": [(0, 46, "PRODUCT"), (65, 70, "MONEY"), (71, 77, "MONEY")]}),
-    ("Sony White In Ear Headphones MDRE9LPW to wishlist$14.95$9.95", {"entities": [(0, 47, "PRODUCT"), (66, 71, "MONEY"), (72, 78, "MONEY")]}),
-    ("Sony Blue In Ear Headphones MDRE9LPL to wishlist$14.95$9.95", {"entities": [(0, 46, "PRODUCT"), (65, 70, "MONEY"), (71, 77, "MONEY")]}),
-    ("Sony Wireless headphones Black WHCH520B to wishlist$99.95$79.95", {"entities": [(0, 51, "PRODUCT"), (70, 75, "MONEY"), (76, 82, "MONEY")]}),
-    ("Sony Black Noise Cancelling Headphones WH1000XM4B to wishlist$549.00$439.00", {"entities": [(0, 55, "PRODUCT"), (74, 79, "MONEY"), (80, 86, "MONEY")]}),
+    ("Add Sony X-Series Portable Wireless Speaker SRSXE300H in Grey to wishlist$319.00$249.00", {"entities": [(4, 61, "ORG"), (76, 81, "MONEY")]}),
+    ("Add Sony X-Series Portable Wireless Speaker SRSXE300B in Black to wishlist$319.00$249.00", {"entities": [(4, 61, "ORG"), (76, 81, "MONEY")]}),
+    ("Add Sony X-Series Portable Wireless Speaker SRSXE200H in Grey to wishlist$249.00$189.00", {"entities": [(4, 60, "ORG"), (75, 80, "MONEY"),]}),
+    ("Add Sony X-Series Portable Wireless Speaker SRSXE200L in Blue to wishlist$249.00$189.00", {"entities": [(4, 60, "ORG"), (74, 79, "MONEY"),]}),
+    ("Add Sony X-Series Portable Wireless Speaker SRSXG300B in Black to wishlist$479.00$383.20", {"entities": [(4, 61, "ORG"), (74, 79, "MONEY")]}),
+    ("Add Sony X-Series Portable Wireless Speaker SRSXG300H in Grey to wishlist$479.00$383.20", {"entities": [(4, 60, "ORG"), (74, 79, "MONEY"),]}),
+    ("Add Sony Linkbuds S WFLS900NB in Black to wishlist$349.95$279.96", {"entities": [(4, 38, "ORG"), (67, 72, "MONEY")]}),
+    ("Add Sony Sony Black On Ear Noise Cancelling Headphones MDRZX110NC to wishlist$99.95$79.95", {"entities": [(4, 65, "ORG"), (92, 97, "MONEY")]}),
+    ("Core Cargo Shorts in Dress Beige Add Superdry", {"entities" : [(0, 32, "ORG")]})
 ]
 
 # Train the model
@@ -39,36 +32,52 @@ for epoch in range(10):
         example = Example.from_dict(nlp.make_doc(text), gold_dict)
         nlp.update([example], drop=0.5)
 
+print(nlp.tokenizer.explain(text))
+
 def scrape_prices(url):
-    response = requests.get(url)
-    soup = BeautifulSoup(response.content, 'html.parser')
+    try:
+        response = requests.get(url)
+        response.raise_for_status()  # Raise an exception for bad responses
+        soup = BeautifulSoup(response.content, 'lxml')
+        all_text = soup.get_text()
 
-    # Find all text on the page
-    all_text = soup.get_text()
+        # Preprocess the text to replace non-breaking space with a regular space
+        all_text = all_text.replace('\xa0', ' ')
 
-    # Use regular expression to find potential price patterns
-    price_patterns = re.findall(r'\$\s?\d+(?:,\d+)*(?:\.\d+)?', all_text)
+        price_patterns = re.findall(r'\$\s?\d+(?:,\d+)*(?:\.\d+)?', all_text)
+        
+        prices = set()
+        links = set()
+        names = set()
 
-    # Process the text using spaCy to filter out non-price entities
-    prices = []
-    links = []
-    names = []
-    for pattern in price_patterns:
-        doc = nlp(pattern)
+        for pattern in price_patterns:
+            doc = nlp(pattern)
+            for ent in doc.ents:
+                if ent.label_ == 'MONEY':
+                    prices.add(ent.text)
+
+        doc = nlp(all_text)
         for ent in doc.ents:
-            if ent.label_ == 'MONEY':
-                prices.append(ent.text)
+            if ent.label_ == 'ORG' :
+                names.add(ent.text)
+                text = ent.text.replace(' ', '-')
+                # Find all <a> tags in the soup
+                all_a_tags = soup.find_all('a')
 
-    doc = nlp(all_text)
-    for ent in doc.ents:
-        if ent.label_ == 'PRODUCT':
-            names.append(ent.text)
-            # Check if there is a link with the product name
-            text = ent.text.replace(' ', '-')
-            link = soup.find('a', {'href': re.compile(fr'{re.escape(text)}', re.I)})
-            links.append(urljoin(url, link.get('href')) if link else None)
+                # Iterate through each <a> tag
+                for a_tag in all_a_tags:
+                    # Get the 'href' attribute of the current <a> tag
+                    href = a_tag.get('href')
 
-    return prices, names, links
+                    # Check if 'href' is not None and if the partial ratio is above a certain threshold (e.g., 70)
+                    if href and fuzz.partial_ratio(text, href) > 70:
+                        # Add the joined URL to the 'links' set
+                        links.add(urljoin("https://www.myer.com.au/", href))
+
+        return prices, names, links
+    except requests.exceptions.RequestException as e:
+        print(f"Error fetching the URL: {e}")
+        return set(), set(), set()
 
 def prices_to_dataframe(names, prices, links):
     # Create a DataFrame with 'name' and 'price' columns
@@ -82,6 +91,14 @@ def analyze_prices(url):
     prices_df = prices_to_dataframe(prices,names,links)
     return prices_df
 
-url = 'https://www.myer.com.au/b/Sony'
+url = 'https://www.myer.com.au/c/men/mens-clothing/mens-shorts'
 prices_df = analyze_prices(url)
 print(prices_df)
+
+
+for text, annotations in TRAIN_DATA:
+    doc = nlp.make_doc(text)
+    tags = offsets_to_biluo_tags(doc, annotations["entities"])
+    print(f"Text: {text}")
+    print(f"Tags: {tags}")
+
